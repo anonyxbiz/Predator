390bcaad71e68d676db6141d554a9a9aea8f67ff
anonyxbiz <anonyxbiz@gmail.com>
2024-11-09 20:25:40 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="Predator",
-    version="1.3.2",
+    version="1.3.3",
     description="Predator.",
     author="Anonyxbiz",
     author_email="anonyxbiz@gmail.com",
